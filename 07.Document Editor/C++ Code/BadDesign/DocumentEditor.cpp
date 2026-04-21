b7710321c6b5de8d61e9debdcaa944754ec9aa6b
singhsatyaprakash <singhsatyaprakash70675@gmail.com>
2026-04-21 19:00:53 +0530

    Document Editor

--- 07.Document Editor/C++ Code/BadDesign/DocumentEditor.cpp
+++ 07.Document Editor/C++ Code/BadDesign/DocumentEditor.cpp
@@ -26,8 +26,7 @@ public:
         if(renderedDocument.empty()) {
             string result;
             for (auto element : documentElements) {
-                if (element.size() > 4 && (element.substr(element.size() - 4) == ".jpg" ||
-                 element.substr(element.size() - 4) == ".png")) {
+                if (element.size() > 4 && (element.substr(element.size() - 4) == ".jpg" || element.substr(element.size() - 4) == ".png")) {
                     result += "[Image: " + element + "]" + "\n";
                 } else {
                     result += element + "\n";
